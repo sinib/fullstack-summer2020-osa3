ded2a741feb1c5f7bba56270e7bf806586f4589b
Sini <hotantenni@gmail.com>
2020-08-11 23:40:29 +0300

    Osan 3 tehtävät 3.7-3.8 lisätty.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token('body', (req, res) => {
+    if(JSON.stringify(req.body) != '{}'){
+        return JSON.stringify(req.body)
+    }
+    return null
+  }
+)
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
