6be2801f67aa76ee5f9625e122fb7b1ab6ee78b7
Sini <hotantenni@gmail.com>
2020-08-27 20:13:51 +0300

    frontend

---
+++ build/precache-manifest.302fc99ae1de5642a8b095d63749f40d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5fa200a5c6e09fbcccf36a12eb8ed41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a3d3694cb4e3e227014",
+    "url": "/static/css/main.0a1fc9d5.chunk.css"
+  },
+  {
+    "revision": "77ed0c26fb7a102b89fb",
+    "url": "/static/js/2.936a527d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.936a527d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3d3694cb4e3e227014",
+    "url": "/static/js/main.7f17c84f.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
