eea843311df3d7cb9e8e6b1086b53dfe1801ca3a
Sini <hotantenni@gmail.com>
2020-10-15 22:01:55 +0300

    Tehtävä 3.22 lisätty.

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 morgan.token('body', (req, res) => {
-    if(JSON.stringify(req.body) != '{}'){
-        return JSON.stringify(req.body)
-    }
-    return null
+  if(JSON.stringify(req.body) !== '{}'){
+    return JSON.stringify(req.body)
   }
+  return null
+}
 )
 
 const app = express()
@@ -18,71 +18,70 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
- 
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>')
+  res.send('<h1>Hello</h1>')
 })
 
 app.get('/info', (req, res) => {
-    const date = Date()
-    Person.countDocuments({},(aa,amount) => {
-        res.send(`<p>Phonebook has ${amount} contacts.</p>
+  const date = Date()
+  Person.countDocuments({},(aa,amount) => {
+    res.send(`<p>Phonebook has ${amount} contacts.</p>
                 <p>${date}</p>`
-        )
-    })
-    
+    )
+  })
 })
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
+  Person.find({})
     .then(guys => {
-        res.json(guys)
+      res.json(guys)
     })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
+  Person.findById(req.params.id)
     .then(guy => {
-        if (guy) {        
-          res.json(guy)      
-        } 
-        else {        
-            res.status(404).end()      
-        }    
+      if (guy) {
+        res.json(guy)
+      }
+      else {
+        res.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const newGuyData = req.body
-    if (!newGuyData.name || !newGuyData.number) {
-        return res.status(400)
-            .json(
-                { 
-                error: 'Name or number is missing!' 
-                }
-            )
-    }
-    const guy = new Person({
-        name: newGuyData.name,
-        number: newGuyData.number,
-    })
+  const newGuyData = req.body
+  if (!newGuyData.name || !newGuyData.number) {
+    return res.status(400)
+      .json(
+        {
+          error: 'Name or number is missing!'
+        }
+      )
+  }
+  const guy = new Person({
+    name: newGuyData.name,
+    number: newGuyData.number,
+  })
 
-    guy.save()
+  guy.save()
     .then(newGuy => {
-        res.json(newGuy)
+      res.json(newGuy)
     })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const newGuyData = req.body
-    const guy = {
-        name: newGuyData.name,
-        number: newGuyData.number,
-    }
-    Person.findByIdAndUpdate(req.params.id, guy, { new: true })
+  const newGuyData = req.body
+  const guy = {
+    name: newGuyData.name,
+    number: newGuyData.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, guy, { new: true })
     .then(newNumber => {
       res.json(newNumber)
     })
@@ -90,7 +89,7 @@ app.put('/api/persons/:id', (req, res, next) => {
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
+  Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
     })
@@ -98,14 +97,15 @@ app.delete('/api/persons/:id', (req, res, next) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({error: 'Id is not valid'})
-    } else if (error.name === 'ValidationError') {
-      return response.status(403).send({error: error.message})
-    }
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Id is not valid' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(403).send({ error: error.message })
+  }
+  next(error)
 }
 app.use(errorHandler)
 
--- models/person.js
+++ models/person.js
@@ -7,16 +7,16 @@ const url = process.env.MONGODB_URI
 
 console.log('Connecting to database', url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => {    
-        console.log('Connection successful.')  
-    })  
-    .catch((error) => {    
-        console.log('Database connection error:', error.message)  
-    })
+  .then(result => {
+    console.log('Connection successful.')
+  })
+  .catch((error) => {
+    console.log('Database connection error:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {type: String, required: true, minlength: 3, unique: true},
-    number: {type: String, required: true, minlength: 8}
+  name: { type: String, required: true, minlength: 3, unique: true },
+  number: { type: String, required: true, minlength: 8 }
 })
 
 personSchema.plugin(uniqueValidator)
